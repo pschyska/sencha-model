7b92a916fd8eea54d026047398c53f2f51427e5d
unknown <Danny@.(none)>
2010-04-15 16:32:36 +0800

    Updated the Shoulda Macro to work with the new achitecture

--- shoulda_macros/macros.rb
+++ shoulda_macros/macros.rb
@@ -1,15 +1,17 @@
 class Test::Unit::TestCase
-    ##
-    # Asserts that the passed list of fields are specified in the whorm_fields call
-    # in the model class.
-    # @fields {Symbols} fields A list of fields
-    #
-  def self.should_have_whorm_fields *fields
+  ##
+  # Asserts that the passed list of fields are specified in the whorm_fields call
+  # in the model class.
+  # @fieldset {Symbol} fieldset (optional, set to :default if missing)
+  # @options {Hash} Should contain a :fields key with an array of fields to check for
+  #
+  def self.should_have_whorm_fields_for_fieldset fieldset = :default, fields = []
     klass = described_type
-    should "have the correct whorm_fields" do
+    should "have the correct extjs_fields" do
+      assert !fields.empty?, "an array of fields must be passed.  Example: (:fieldset_name, [:name, :id])"
       fields.each do |field|
-        found_record = klass.whorm_record_fields.find do|record_field|
-          record_field[:name] == field.to_s
+        found_record = klass.whorm_get_fields_for_fieldset(fieldset).find do|record_field|
+          record_field[:name].to_sym == field.to_sym
         end
         assert_not_nil found_record, "whorm field #{field} isn't listed in the #{klass.name} model"
       end
